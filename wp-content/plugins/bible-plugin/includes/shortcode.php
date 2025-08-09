83e0e9201c3276fadf84787eccf36bbca669225e
priestAmbrose <alexey.sitalo@gmail.com>
2025-08-09 01:25:19 -0400

    removed redundant code

--- wp-content/plugins/bible-plugin/includes/shortcode.php
+++ wp-content/plugins/bible-plugin/includes/shortcode.php
@@ -9,9 +9,3 @@ require_once plugin_dir_path(__FILE__) . 'shortcodes/after-pentecost.php';
 require_once plugin_dir_path(__FILE__) . 'shortcodes/calendar-view.php';
 require_once plugin_dir_path(__FILE__) . 'shortcodes/daily-content.php';
 
-function my_daily_content_shortcode() {
-  $todayContent = bible_plugin_after_pentecost_label(date('Y-m-d'));
-
-  return "<div id='bible-day-content'>{$todayContent}</div>";
-}
-add_shortcode('daily_content', 'my_daily_content_shortcode');
