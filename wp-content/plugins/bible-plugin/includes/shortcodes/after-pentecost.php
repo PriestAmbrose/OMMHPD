7416bdb964c64c4c9268bcfd71e399359dbb82c6
priestAmbrose <alexey.sitalo@gmail.com>
2025-09-20 03:17:16 -0400

    preparing printing out psalms

--- wp-content/plugins/bible-plugin/includes/shortcodes/after-pentecost.php
+++ wp-content/plugins/bible-plugin/includes/shortcodes/after-pentecost.php
@@ -3,18 +3,37 @@ if ( ! defined( 'ABSPATH' ) ) {
   exit;
 }
 
+
+
+
 function bp_after_pentecost_shortcode() {
-  $pentecost_date = new DateTime('2025-06-08'); // Example date for Orthodox Pentecost
-  $today = new DateTime(current_time('Y-m-d'));
+  $url = "https://azbyka.ru/biblia/?Ps.9:5-6&utfcs";
 
-  $interval = $pentecost_date->diff($today);
-  $days_after = $interval->invert ? 0 : $interval->days;
+  // Load HTML
+  $html = file_get_contents($url);
 
-  if ($days_after === 0) {
-    return "Today is Pentecost.";
+  // Parse DOM
+  $dom = new DOMDocument();
+  libxml_use_internal_errors(true);
+  $dom->loadHTML('<?xml encoding="utf-8" ?>' . $html);
+  libxml_clear_errors();
+
+  $xpath = new DOMXPath($dom);
+  $nodes = $xpath->query("//*[contains(concat(' ', normalize-space(@class), ' '), ' crossref-verse ')]");
+
+  // Wrap verses in lang-utfcs
+  $output = "<div class='lang-utfcs psalm-verses-cs'>";
+  foreach ($nodes as $node) {
+    $verseText = trim($node->textContent);
+    $output .= "<div class='psalm-verse-cs'>" . htmlspecialchars($verseText) . "</div>";
   }
+  $output .= "</div>";
 
-  return "Today is Day {$days_after} after Pentecost.";
+  return $output;
 }
+
+
+
+
 
 add_shortcode('after_pentecost', 'bp_after_pentecost_shortcode');
