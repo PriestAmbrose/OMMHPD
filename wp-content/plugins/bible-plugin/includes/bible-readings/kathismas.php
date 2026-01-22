fd91662700f6f024608c86b85f4f55f3a70282c7
priestAmbrose <alexey.sitalo@gmail.com>
2026-01-22 00:17:34 -0400

    collected all kathismas

--- wp-content/plugins/bible-plugin/includes/bible-readings/kathismas.php
+++
@@ -1,3 +0,0 @@
-<?php
-return array (
-class="chapter-href">18</a></li><li><a href="/biblia/?Ps.134-142&amp;utfcs" class="chapter-href">19</a></li><li><a href="/biblia/?Ps.143-150&amp;utfcs" class="chapter-href">20</a></li></ul>
